00d56177990eded084562d5b59824c06218f3ed2
Aijin <115892544+Yerbo71@users.noreply.github.com>
2024-02-29 20:53:12 +0600

    commit

--- log-app/src/components/Login.jsx
+++ log-app/src/components/Login.jsx
@@ -1,60 +1,81 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useFormik } from "formik";
 import { Link } from "react-router-dom";
 import "./../components/Login.css";
+import axios from "axios";
 
+export default function Login() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async (values) => {
+      try {
+        const res = await axios.post("http://localhost:8080/api/v1/auth/authenticate", values);
+        console.log("Login success: ", res);
+      } catch (e) {
+        console.log("Login failed: ", e);
+      }
+    },
+  });
 
-export default function Login () {
-    const formik = useFormik({
-        initialValues:{
-            email:"",
-            password:"",
-        },
-    });
-    return(
-        <div className="main2">
-            <div className="login-container">
-                <div className="login-container2">
-                    <h1>Welcome back, User</h1>
-                    <p>Welcome back! Please enter your details.</p>
-                    <button className="btn-google2">Sign up with Google</button>
-                    <div className="or">
-                        <hr className="hrfirst"/>
-                        <div>or</div>
-                        <hr className="hrfirst"/>
-                    </div>
+  useEffect(() => {
+    // If you need any logic when the component mounts, you can put it here.
+  }, []);
 
-                    <form onSubmit={formik.handleSubmit} className="form">
-                        <input 
-                        id="email"
-                        name="email"
-                        type="email"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                        className="input-log" placeholder="user123@mail.com"
-                        />
+  return (
+    <div className="main2">
+      <div className="login-container">
+        <div className="login-container2">
+          <h1>Welcome back, User</h1>
+          <p>Welcome back! Please enter your details.</p>
+          <button className="btn-google2">Sign up with Google</button>
+          <div className="or">
+            <hr className="hrfirst" />
+            <div>or</div>
+            <hr className="hrfirst" />
+          </div>
 
-                        <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                        className="input-log" placeholder="********"
-                        />
-                        <div className="checkandforgot">
-                            <div className="remember">
-                                <input  type="checkbox" id="checked"/>
-                                <label>Remember me for 30 days</label>
-                            </div>
-                            <a href="#">Forgot Password</a>
-                        </div>
-                        <button type="submit" className="btn-sub">Log in</button>
-                    </form>
-                    <div className="linktoregister">Dont`t have an account ? <Link to="/register" className="registerlink">Sign up for free</Link></div>
-                </div>
+          <form onSubmit={formik.handleSubmit} className="form">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              className="input-log"
+              placeholder="user123@mail.com"
+            />
+
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              className="input-log"
+              placeholder="********"
+            />
+            <div className="checkandforgot">
+              <div className="remember">
+                <input type="checkbox" id="checked" />
+                <label>Remember me for 30 days</label>
+              </div>
+              <a href="#">Forgot Password</a>
             </div>
+            <button type="submit" className="btn-sub">
+              Log in
+            </button>
+          </form>
+          <div className="linktoregister">
+            Don't have an account ?{" "}
+            <Link to="/register" className="registerlink">
+              Sign up for free
+            </Link>
+          </div>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }
--- log-app/src/components/Register.jsx
+++ log-app/src/components/Register.jsx
@@ -1,68 +1,91 @@
-import React from "react";
-import { Formik, Form, Field } from "formik"
-import { Link } from "react-router-dom"
+import React, { useEffect } from "react";
+import { Formik, Form, Field } from "formik";
+import { Link } from "react-router-dom";
+import axios from "axios"; // Don't forget to import axios
 import "./Register.css";
+
 function validateEmail(value) {
-    let error;
-    if(!value){
-        error = "Required";
-    }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)){
-        error = "Invalid email address";
-    }
-    return error;
-    }
-function validatePassword(value){
-    let error;
-    if(!value || value.length < 8){
-        error ="Must be at least 8 characters"
-    }
-    return error;
-}
-function validateName(value){
-    let error;
-    if(!value){
-        error = "Required";
-    }
-    return error;
+  let error;
+  if (!value) {
+    error = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    error = "Invalid email address";
+  }
+  return error;
 }
 
-export default function Register () {
+function validatePassword(value) {
+  let error;
+  if (!value || value.length < 8) {
+    error = "Must be at least 8 characters";
+  }
+  return error;
+}
 
-    return(
-        <div className="main">
-            <div className="part-two">
-                <div className="two-container">
-                <h1>Create an account</h1>
-                <p>Let`s get started with your 30 days free trial.</p>
-                    <Formik
-                    initialValues={{
-                        name: "",
-                        email:"",
-                        password:"",
-                    }}
-                    onSubmit={values =>{
-                        console.log(values);
-                    }}
-                    >
-                    {({ errors, touched}) =>(
-                        <Form className="form-container">
-                            <Field name="name" validate={validateName} className="input-field" placeholder=" Name"/>
-                            {errors.name && touched.name && <div className="error">{errors.name}</div>}
+function validateName(value) {
+  let error;
+  if (!value) {
+    error = "Required";
+  }
+  return error;
+}
 
-                            <Field name="email" validate={validateEmail}className="input-field"  placeholder=" Email"/>
-                            {errors.email && touched.email && <div className="error">{errors.email}</div>}
+export default function Register() {
+  const getRegistration = async (values) => {
+    try {
+      const res = await axios.post("http://localhost:8080/api/v1/auth/register", values);
+      console.log("Registration success: ", res);
+    } catch (e) {
+      console.log("Registration failed: ", e);
+    }
+  };
+//   useEffect(()=>{
+//     getRegistration();
+//   })
 
-                            <Field name="password" validate={validatePassword}className="input-field"  placeholder=" Password"/>
-                            {errors.password && touched.password && <div className="error">{errors.password}</div>}
+  return (
+    <div className="main">
+      <div className="part-two">
+        <div className="two-container">
+          <h1>Create an account</h1>
+          <p>Let's get started with your 30 days free trial.</p>
+          <Formik
+            initialValues={{
+              firstname: "",
+              lastname: "",
+              email: "",
+              password: "",
+            }}
+            onSubmit={(values) => {
+              getRegistration(values);
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form className="form-container">
+                <Field name="firstname" validate={validateName} className="input-field" placeholder="First Name" />
+                {errors.firstname && touched.firstname && <div className="error">{errors.firstname}</div>}
 
-                            <button type="submit" className="btn-sub">Create account</button>
-                        </Form>
-                    )}
-                    </Formik>
-                    <button className="btn-google">Sign up with Google</button>
-                    <div className="link-login">Already have an account ? <Link to="/" className="loginlink">Log in</Link></div>
-                </div>
-            </div>
+                <Field name="lastname" validate={validateName} className="input-field" placeholder="Last Name" />
+                {errors.lastname && touched.lastname && <div className="error">{errors.lastname}</div>}
+
+                <Field name="email" validate={validateEmail} className="input-field" placeholder="Email" />
+                {errors.email && touched.email && <div className="error">{errors.email}</div>}
+
+                <Field name="password" validate={validatePassword} className="input-field" placeholder="Password" />
+                {errors.password && touched.password && <div className="error">{errors.password}</div>}
+
+                <button type="submit" className="btn-sub">
+                  Create account
+                </button>
+              </Form>
+            )}
+          </Formik>
+          <button className="btn-google">Sign up with Google</button>
+          <div className="link-login">
+            Already have an account? <Link to="/" className="loginlink">Log in</Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
